3013c6e9d547769b6773151bd388cc6ef95ebb5c
Fran <franbarreraroman0@gmail.com>
2025-11-04 16:37:23 +0100

    Commit ´definitivo´ fran

---
+++ cabe-zon_back-end/src/main/java/com/safa/cabezon_backend/CabeZonBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.safa.cabezon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeZonBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabeZonBackEndApplication.class, args);
+    }
+
+}
---
+++ cabe-zon_back-end/src/test/java/com/safa/cabezon_backend/CabeZonBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.cabezon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeZonBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
