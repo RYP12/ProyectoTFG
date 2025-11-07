fc8d6b956325617f21d7f827176a54a24ba24737
IvanRVProjects <iramosvillegas@safareyes.es>
2025-11-07 20:56:06 +0100

    Cifrado de contraseñas mediante bcrypt

--- cabe-zon_back-end/src/main/java/com/safa/cabezon_backend/Servicios/ClienteServicie.java
+++ cabe-zon_back-end/src/main/java/com/safa/cabezon_backend/Servicios/ClienteServicie.java
@@ -5,6 +5,8 @@ import com.safa.cabezon_backend.Modelos.Cliente;
 import com.safa.cabezon_backend.Repositorios.IClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,23 +21,26 @@ public class ClienteServicie {
 
     public Cliente BuscarClientePorId(Integer id){return clienteRepository.findById(id).orElse(null);}
 
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void CrearCliente(ClienteDTO clienteDto){
         Cliente nuevoCliente = new Cliente();
         nuevoCliente.setNombre(clienteDto.getNombre());
         nuevoCliente.setApellidos(clienteDto.getApellidos());
         nuevoCliente.setCorreo(clienteDto.getCorreo());
-        nuevoCliente.setPasswordHash(clienteDto.getPasswordHash());
+        nuevoCliente.setPasswordHash(passwordEncoder.encode(clienteDto.getPasswordHash()));
         clienteRepository.save(nuevoCliente);
     }
 
     public void EliminarClientePorId(Integer id){ clienteRepository.deleteById(id);}
 
     public void EditarClientePorId(Integer id, ClienteDTO clienteDto){
+
         Cliente cliente = clienteRepository.findById(id).orElse(null);
         cliente.setNombre(clienteDto.getNombre());
         cliente.setApellidos(clienteDto.getApellidos());
         cliente.setCorreo(clienteDto.getCorreo());
-        cliente.setPasswordHash(clienteDto.getPasswordHash());
+        cliente.setPasswordHash(passwordEncoder.encode(clienteDto.getPasswordHash()));
         cliente.setFoto(clienteDto.getFoto());
         cliente.setRol(clienteDto.getRol());
         clienteRepository.save(cliente);
