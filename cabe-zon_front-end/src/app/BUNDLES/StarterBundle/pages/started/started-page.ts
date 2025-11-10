5dc706ee63aeaa2517af419b76966a50a0148359
RYP12 <ramonye2002@gmail.com>
2025-11-10 20:29:45 +0100

    arreglo sebas

--- cabe-zon_front-end/src/app/BUNDLES/StarterBundle/pages/started/started-page.ts
+++ cabe-zon_front-end/src/app/BUNDLES/StarterBundle/pages/started/started-page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-started-page',
+  selector: 'app-started',
   imports: [],
   templateUrl: './started-page.html',
   styleUrl: './started-page.css',
--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Catalogo} from './BUNDLES/CatalogoBundle/pages/catalogo/catalogo';
+import {Header} from './SHARED/header/header';
+import {EdicionLimitada} from './BUNDLES/EdicionLimitadaBundle/pages/edicion-limitada/edicion-limitada';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Catalogo, Header, EdicionLimitada],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
