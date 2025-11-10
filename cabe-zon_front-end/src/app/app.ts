a7c3cb98fcc4cff125e1a2f32e07231a06985b5d
Fran <franbarreraroman0@gmail.com>
2025-11-10 19:16:37 +0100

    Arreglo problema owner-footer

--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -1,22 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import {Footer} from './SHARED/footer/footer';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Footer],
-=======
-import { Propietario } from "./BUNDLES/OwnerBundle/pages/owner/propietario";
 import { OwnerControl } from "./BUNDLES/OwnerBundle/pages/owner-control/owner-control";
+import { Footer } from './SHARED/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OwnerControl],
->>>>>>> franLocal
+  standalone: true,
+  imports: [RouterOutlet, OwnerControl, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
+
 export class App {
   protected readonly title = signal('cabe-zon_front-end');
 }
