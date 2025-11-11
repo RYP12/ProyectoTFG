56d8a00ef5f5270cdc3ab3668fce6b5a04a18c19
cvas-ctrl <saramayoayarde@safareyes.es>
2025-11-11 17:57:09 +0100

    FUNKO, VALORACIONES Y RESEÑAS HECHO

---
+++ cabe-zon_front-end/src/app/BUNDLES/FunkoBundle/pages/funko/funko.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funko',
+  imports: [],
+  templateUrl: './funko.html',
+  styleUrl: './funko.css',
+})
+export class Funko {
+
+}
--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -2,12 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Footer} from './SHARED/footer/footer';
 import { OwnerControl } from "./BUNDLES/OwnerBundle/pages/owner-control/owner-control";
+import {Catalogo} from './BUNDLES/CatalogoBundle/pages/catalogo/catalogo';
+import {Header} from './SHARED/header/header';
+import {Funko} from './BUNDLES/FunkoBundle/pages/funko/funko';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OwnerControl, Footer],
+  imports: [RouterOutlet, OwnerControl, Footer, Catalogo, Header, Funko],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
