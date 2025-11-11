694e2853861de0b3bc868b505eb34f3f0ccd7f67
RYP12 <ramonye2002@gmail.com>
2025-11-11 20:31:31 +0100

    cliente/parte1

---
+++ cabe-zon_front-end/src/app/BUNDLES/OwnerBundle/pages/owner-control-cliente-form/owner-control-cliente-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-control-cliente-form',
+  imports: [],
+  templateUrl: './owner-control-cliente-form.html',
+  styleUrl: './owner-control-cliente-form.css',
+})
+export class OwnerControlClienteForm {
+
+}
--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -2,12 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Footer} from './SHARED/footer/footer';
 import { OwnerControl } from "./BUNDLES/OwnerBundle/pages/owner-control/owner-control";
+import {
+  OwnerControlClienteForm
+} from './BUNDLES/OwnerBundle/pages/owner-control-cliente-form/owner-control-cliente-form';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OwnerControl, Footer],
+  imports: [RouterOutlet, OwnerControl, Footer, OwnerControlClienteForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
