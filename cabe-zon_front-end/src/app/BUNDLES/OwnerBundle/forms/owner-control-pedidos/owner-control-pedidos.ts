026f06806efe07ca526a7c7577e1b542c553832b
Fran <franbarreraroman0@gmail.com>
2025-11-13 20:29:16 +0100

    empezando owner-control-pedidos

---
+++ cabe-zon_front-end/src/app/BUNDLES/OwnerBundle/forms/owner-control-pedidos/owner-control-pedidos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-control-pedidos',
+  imports: [],
+  templateUrl: './owner-control-pedidos.html',
+  styleUrl: './owner-control-pedidos.css',
+})
+export class OwnerControlPedidos {
+
+}
--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -5,12 +5,13 @@ import { Footer } from './SHARED/footer/footer';
 import {
   OwnerControlProductoForm
 } from './BUNDLES/OwnerBundle/forms/owner-control-producto-form/owner-control-producto-form';
+import {OwnerControlPedidos} from './BUNDLES/OwnerBundle/forms/owner-control-pedidos/owner-control-pedidos';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OwnerControlPedidoForm, Footer, OwnerControlProductoForm],
+  imports: [RouterOutlet, OwnerControlPedidoForm, Footer, OwnerControlProductoForm, OwnerControlPedidos],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
