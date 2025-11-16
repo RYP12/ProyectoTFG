e72d43ab2b471d77b3190537228656319506d7d3
cvas-ctrl <saramayoayarde@safareyes.es>
2025-11-16 19:59:50 +0100

    DOCUMENTACION CARRITO,CATALOGO,EDI.LIMITADA,FINALIZAR Y FUNKO

--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Footer} from './SHARED/footer/footer';
+import {Header} from './SHARED/header/header';
+import {Carrito} from './BUNDLES/CarritoBundle/pages/carrito/carrito';
+import {Catalogo} from './BUNDLES/CatalogoBundle/pages/catalogo/catalogo';
+import {Confirmar} from './BUNDLES/ConfirmacionPedidoBundle/pages/confirmar/confirmar';
+import {EdicionLimitada} from './BUNDLES/EdicionLimitadaBundle/pages/edicion-limitada/edicion-limitada';
+import {Finalizar} from './BUNDLES/FinalizarPedidoBundle/pages/finalizar/finalizar';
+import {Funko} from './BUNDLES/FunkoBundle/pages/funko/funko';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, Header, Carrito, Catalogo, Confirmar, EdicionLimitada, Finalizar, Funko],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
