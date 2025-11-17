6da8e13a260f311950a7b009d303996e02e4b094
Fran <franbarreraroman0@gmail.com>
2025-11-17 21:00:10 +0100

    retocando owner bundle

--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Footer} from './SHARED/footer/footer';
+import {
+  OwnerControlClienteForm
+} from './BUNDLES/OwnerBundle/pages/owner-control-cliente-form/owner-control-cliente-form';
+import {OwnerControlProductos} from './BUNDLES/OwnerBundle/forms/owner-control-productos/owner-control-productos';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, OwnerControlClienteForm, OwnerControlProductos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
