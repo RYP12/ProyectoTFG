fe68d0551dd90e30af2f96535566e988ee8f100c
cvas-ctrl <saramayoayarde@safareyes.es>
2025-11-17 21:01:23 +0100

    ACTUALIZACION DE HEADER, CATALOGO, ED.LIMITADA Y ERROR

--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -8,12 +8,18 @@ import {Confirmar} from './BUNDLES/ConfirmacionPedidoBundle/pages/confirmar/conf
 import {EdicionLimitada} from './BUNDLES/EdicionLimitadaBundle/pages/edicion-limitada/edicion-limitada';
 import {Finalizar} from './BUNDLES/FinalizarPedidoBundle/pages/finalizar/finalizar';
 import {Funko} from './BUNDLES/FunkoBundle/pages/funko/funko';
+import {
+  OwnerControlClienteForm
+} from './BUNDLES/OwnerBundle/pages/owner-control-cliente-form/owner-control-cliente-form';
+import {OwnerControlPedidos} from './BUNDLES/OwnerBundle/forms/owner-control-pedidos/owner-control-pedidos';
+import {OwnerControlProductos} from './BUNDLES/OwnerBundle/forms/owner-control-productos/owner-control-productos';
+import {Error} from './SHARED/ErrorBundle/pages/error/error';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header, Carrito, Catalogo, Confirmar, EdicionLimitada, Finalizar, Funko],
+  imports: [RouterOutlet, Footer, Header, Carrito, Catalogo, Confirmar, EdicionLimitada, Finalizar, Funko, OwnerControlClienteForm, OwnerControlPedidos, OwnerControlProductos, Error],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
