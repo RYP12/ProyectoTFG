a72931562a5bc67372f96cfdb46ea4da320533c5
RYP12 <ramonye2002@gmail.com>
2025-11-18 16:32:57 +0100

    actualizacion18/11

--- cabe-zon_front-end/src/app/app.ts
+++ cabe-zon_front-end/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Footer} from './SHARED/footer/footer';
+import {
+  OwnerControlClienteForm
+} from './BUNDLES/OwnerBundle/forms/owner-control-cliente-form/owner-control-cliente-form';
+import {General} from './BUNDLES/GeneralBundle/pages/general/general';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, OwnerControlClienteForm, General],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
