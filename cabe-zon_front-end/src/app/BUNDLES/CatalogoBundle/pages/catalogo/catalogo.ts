d0c2ec5b65fbcfe828f87368591ded5729643958
Fran <franbarreraroman0@gmail.com>
2025-11-19 19:08:23 +0100

    Comprobando data service

--- cabe-zon_front-end/src/app/BUNDLES/CatalogoBundle/pages/catalogo/catalogo.ts
+++ cabe-zon_front-end/src/app/BUNDLES/CatalogoBundle/pages/catalogo/catalogo.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Data, Producto} from '../../../../SERVICES/data';
 
 @Component({
   selector: 'app-catalogo',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css',
 })
-export class Catalogo {
+export class Catalogo implements OnInit {
+  listaProductos: Producto[] = [];
 
+  constructor(private dataService: Data) { }
+
+  ngOnInit() {
+    this.dataService.obtenerProductos().subscribe({
+      next: (datos) => {
+        console.log(datos);
+        this.listaProductos = datos;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 }
--- cabe-zon_front-end/src/app/SERVICES/data.ts
+++ cabe-zon_front-end/src/app/SERVICES/data.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import {Routes} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+
+export interface Producto {
+  id: number;
+  name: string;
+  price: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class Data {
   private apiUrl: string = 'http://localhost:8080';
 
   constructor(private http: HttpClient) {}
 
+  obtenerProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(`${this.apiUrl}/producto/all`);
+  }
 }
