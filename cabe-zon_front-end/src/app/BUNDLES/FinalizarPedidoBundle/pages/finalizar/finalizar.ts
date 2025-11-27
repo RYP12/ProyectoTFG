db8290e641a344a01a8acfc1d24a15b02a18c340
Fran <franbarreraroman0@gmail.com>
2025-11-27 17:43:44 +0100

    Avanzando

--- cabe-zon_front-end/src/app/BUNDLES/FinalizarPedidoBundle/pages/finalizar/finalizar.ts
+++ cabe-zon_front-end/src/app/BUNDLES/FinalizarPedidoBundle/pages/finalizar/finalizar.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Header} from '../../../../SHARED/header/header';
 import {Footer} from '../../../../SHARED/footer/footer';
 import {Producto} from '../../../../SERVICES/productoService';
@@ -16,5 +16,32 @@ import {Router} from '@angular/router';
   styleUrl: './finalizar.css',
 })
 export class Finalizar  {
+  private carritoService = inject(CarritoService);
+  private router = inject(Router);
 
+  // 1. Exponemos las señales del servicio (Read-only)
+  items = this.carritoService.productosCarrito$;
+
+  // Lógica para el botón de "Pagar" o "Confirmar"
+  confirmarPedido() {
+    // Aquí iría la lógica para enviar al Backend (PedidoService)
+    console.log('Procesando pedido con:', this.items);
+    alert('Funcionalidad de pago en construcción');
+  }
+
+  volverAlCatalogo() {
+    this.router.navigate(['/catalogo']);
+  }
+
+  protected decrementarCantidad(funko: any) {
+    
+  }
+
+  protected incrementarCantidad(funko: any) {
+    
+  }
+
+  protected eliminarDelPedido(funko: any) {
+    
+  }
 }
