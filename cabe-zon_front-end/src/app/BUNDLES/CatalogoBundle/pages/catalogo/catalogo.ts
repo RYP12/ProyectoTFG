abe77ed2dae28a050be26242a4664d6941d7d341
RYP12 <ramonye2002@gmail.com>
2025-12-01 20:31:40 +0100

    1/12 MERGE1

--- cabe-zon_front-end/src/app/BUNDLES/CatalogoBundle/pages/catalogo/catalogo.ts
+++ cabe-zon_front-end/src/app/BUNDLES/CatalogoBundle/pages/catalogo/catalogo.ts
@@ -2,16 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {ProductoService, Producto} from '../../../../SERVICES/productoService';
 import {Header} from '../../../../SHARED/header/header';
 import {Footer} from '../../../../SHARED/footer/footer';
-import {FormsModule} from '@angular/forms';
-import {FiltrosService} from '../../../../SERVICES/filtros-service';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-catalogo',
   imports: [
     Header,
-    Footer,
-    FormsModule
+    Footer
   ],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css',
@@ -30,50 +26,58 @@ export class Catalogo implements OnInit {
 
   constructor(private productoService: ProductoService) { }
 
-  // FUNCIONES QUE SE EJECUTAN AL INICIAR LA PAGINA
   ngOnInit() {
-    this.cargarTodos();
-  }
-  // MUESTRA TODOS LOS PRODUCTOS
-  cargarTodos() {
     this.productoService.obtenerProductos().subscribe({
-      next: (datos) => this.listaProductos = datos,
-      error: (err) => console.error(err)
-    });
+      next: (datos) => {
+        this.listaProductos = datos;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
-
 
   aplicarFiltros (){
 
-    let busquedaActiva = false;
-    let min: number = 0;
-    let max: number = 10000;
+    // RANGOS DE PRECIO
 
-    // SI HAY UN RANGO SELECCIONADO, DEFINIMOS MAXIMOS Y MINIMOS
-    if (this.filtros.rangoPrecio !== '') {
-      busquedaActiva = true;
-      switch (this.filtros.rangoPrecio) {
-        case '0€ - 25€': min = 0; max = 25; break;
-        case '25€ - 50€': min = 25; max = 50; break;
-        case '50€ - 1000€': min = 50; max = 1000; break;
-        default: busquedaActiva = false; break;
-      }
+    let minimo: number | undefined;
+    let maximo: number | undefined;
+
+    switch (this.filtros.rangoPrecio) {
+      case '0€ - 25€':
+        minimo = 0;
+        maximo = 25;
+        break;
+      case '25€ - 50€':
+        minimo = 25;
+        maximo = 50;
+        break;
+      case '50€ - 1000€':
+        minimo = 50;
+        maximo = 1000;
+        break;
+
+      default:
+        // Si no se selecciona nada se queda 'undefined' los valores
+        break;
     }
 
     const parametrosBack = {
       orden: this.filtros.orden,
-      minimo: min,
-      maximo: max,
+      minimo: minimo,
+      maximo: maximo,
       colaboracion: this.filtros.colaboracion
     }
 
-
-
     console.log('Enviando al backend:', parametrosBack);
 
+
+
+
+
+
   }
-
-
   // METODO TEMPORAL PARA ARRANCAR EL PROYECTO
   protected agregarAlCarrito(funko: Producto) {
 
