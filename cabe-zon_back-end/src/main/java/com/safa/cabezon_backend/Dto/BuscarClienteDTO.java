449dbcd476226282041a2b63f8ef22907b777fe4
ivanramoss-safa <iramosvillegas@safareyes.es>
2025-12-10 15:17:40 +0100

    Update BuscarClienteDTO.java

--- cabe-zon_back-end/src/main/java/com/safa/cabezon_backend/Dto/BuscarClienteDTO.java
+++ cabe-zon_back-end/src/main/java/com/safa/cabezon_backend/Dto/BuscarClienteDTO.java
@@ -14,5 +14,5 @@ public class BuscarClienteDTO {
     private Integer cabecoins;
     private String email;
     private Nivel nivel;
-    private List<BuscarPedidoDTO> pedidos;
+    private List<PedidoSimpleDTO> pedidos;
 }
